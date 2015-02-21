772b78f1bcc0204cf20568e1ebd01979b5a1a8df
aherrera17 <aherrera17@hawkmail.hccfl.edu>
2015-02-21 16:33:06 -0500

    Haul

--- SearchEngineGit/src/Main.java
+++ SearchEngineGit/src/Main.java
@@ -8,185 +8,144 @@
  *
  */
 
-package PopCultureReference;
+
+package popculture;
+
+import java.awt.EventQueue;
 
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.JFrame;
+import java.awt.Panel;
+import java.awt.BorderLayout;
+import java.awt.Label;
+import java.awt.TextArea;
+import java.awt.TextField;
+import java.awt.Button;
+import java.awt.List;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Checkbox;
 
+public class SearchEngine {
 
-public class Main extends Frame {
-	
-	public Main (){
-		
-		
-		setTitle("Search Engine");
-		setSize ( 600 , 400);
-		setBackground(Color.white);
-		setLocation (500,500);
-		addWindowListener( new WindowAdapter()
-		{
-			public void windowClosing( WindowEvent e)
-			{
-				System.exit(0);
-			}
-		});
-		
-		//layout
-		
-		setLayout ( new BorderLayout() );
-		//creates new pannel
-		Panel top = new Panel();
-		//add panel to north of layout
-		add(top, BorderLayout.PAGE_START);
-		Label searchTxt = new Label("Search Terms");
-		TextField search = new TextField("",28);
-		Button searchBtn = new Button("Search");
-		
-		top.add(searchTxt);
-		top.add (search);
-		top.add(searchBtn);
-		// create new text area
-		TextArea searchResult = new TextArea("",30, 30, TextArea.SCROLLBARS_NONE);
-		searchResult.setEditable(false);
-		add(searchResult, BorderLayout.CENTER);
-		
-		// Bar underneath the Search Textbox
-		Panel middle = new Panel();
-		add (middle, BorderLayout.PAGE_END);
-		Label searchType = new Label ("Search Type: ");
-		
-		//AWT does not allow for radio buttons so checkboxes are used
-		CheckboxGroup choices = new CheckboxGroup();
-		Checkbox searchTerms = new Checkbox("Search Terms", choices, true);
-		Checkbox anySearch = new Checkbox("Any Search Terms", choices, true);
-		Checkbox exactPhrase = new Checkbox("Exact Phrase", choices, true);
-		
-		middle.add(searchType);
-		middle.add(searchTerms);
-		middle.add(anySearch);
-		middle.add(exactPhrase);
-		
-		
-		// Menu Bar
-		
-		MenuBar menu = new MenuBar();
-		setMenuBar(menu);
-		
-		// Menu Choices
-		Menu fileMenu = new Menu("File");
-		Menu helpMenu = new Menu ("Help");
-		
-		
-		
-		// File Menu
-			menu.add(fileMenu);
-			MenuItem maint = new MenuItem("Maintenance"); //Creates Menu Item for Maintenance
-			MenuItem exit = new MenuItem("Exit");
-			
-			fileMenu.add(maint); 
-			fileMenu.add(exit);
-			
-		// Help Menu
-			menu.add(helpMenu);
-			MenuItem viewHelp = new MenuItem("View Help");
-			MenuItem about = new MenuItem("About");
-			
-			helpMenu.add(viewHelp);
-			helpMenu.add(about);
-			
-		// Action Listeners for File and Help Commands
-			maint.addActionListener( new ActionListener()
-			{
-				public void actionPerformed (ActionEvent e)
-				{
-					Frame maintenance = new Main("maintenance");
-				
-				}
-			}); 
-		
-		
-		   viewHelp.addActionListener (new ActionListener()
-		   {
-				public void actionPerformed (ActionEvent e)
-				{
-				    Frame help = new Main("Help");
-				}
-			});
-			
-			
-		
-		
-		
-		
-	}
+	private JFrame searchGUI;
 
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Frame frame = new Main();
-		frame.setVisible( true );
-	}
-	
-	// Maintenance Window
-
-	public Main ( String maintenance){
-		setTitle("Search Engine");
-		setSize ( 600 , 400);
-		setBackground(Color.white);
-		//setLocation ( 600, 600);
-		addWindowListener( new WindowAdapter()
-		{
-			public void windowClosing( WindowEvent e)
-			{
-				dispose(); //Closes current window without closing whole program
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SearchEngine window = new SearchEngine();
+					window.searchGUI.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		});
-		
+	}
 
+	/**
+	 * Create the application.
+	 */
+	public SearchEngine() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		searchGUI = new JFrame();
+		searchGUI.setTitle("Search Engine");
+		searchGUI.setBounds(100, 100, 600, 400);
+		searchGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		searchGUI.getContentPane().setLayout(new BorderLayout(0, 0));
 		
-		//layout
-		
-		
-		setLayout ( new BorderLayout() );
-		Panel top = new Panel();
-		//add panel to north of layout
-		add(top, BorderLayout.NORTH);
-		Panel flowNorth = new Panel();
-		top.add(flowNorth, BorderLayout.NORTH);
-		Label searchMaintenance = new Label("Search Engine- Index");
-		flowNorth.add(searchMaintenance);
-		Panel flowCenter = new Panel();
-		add(flowCenter, BorderLayout.CENTER);
-		Button addFile = new Button("Add file");
-		flowCenter.add(addFile);
-		Button rebuild = new Button("Rebuild Out of date");
-		flowCenter.add(rebuild);
-		Button remove = new Button("Remove Selected Files");
-		flowCenter.add(remove);
-		TextArea maint = new TextArea("",20,10,TextArea.SCROLLBARS_NONE); //Find better way to increase size then adding more rows and columns
-		maint.setEditable(false);
-		add(maint, BorderLayout.SOUTH);
+		/* Menu Bar
+		 * 
+		 * 
+		 */
 		
 		
+		JMenuBar menuBar = new JMenuBar();
+		searchGUI.setJMenuBar(menuBar);
 		
-		setVisible(true);
+		/* File Menu containing Maintenance and Exit Menu Options */
+		
+		JMenu fileMenu = new JMenu("File");
+		menuBar.add(fileMenu);
+		
+		JMenuItem maintenanceItem = new JMenuItem("Maintenance");
+		fileMenu.add(maintenanceItem);
+		
+		JMenuItem exitItem = new JMenuItem("Exit");
+		fileMenu.add(exitItem);
+		
+		/* Help Menu Bar containing View Help and About Menu Options */
+		
+		JMenu helpMenu = new JMenu("Help");
+		menuBar.add(helpMenu);
+		
+		JMenuItem viewHelpItem = new JMenuItem("View Help");
+		helpMenu.add(viewHelpItem);
+		
+		JMenuItem aboutItem = new JMenuItem("About");
+		helpMenu.add(aboutItem);
+		
+		/*
+		 * 
+		 */
+		
+		/* Search Results Panel */
+		
+		Panel resultPanel = new Panel();
+		searchGUI.getContentPane().add(resultPanel);
+		
+			TextArea resultArea = new TextArea("",18, 90, TextArea.SCROLLBARS_NONE);
+			resultArea.setForeground(Color.WHITE);
+			resultArea.setEditable(false);
+			resultPanel.add(resultArea);
+		
+		/* Top Bar */
+		
+		JPanel searchBarPanel = new JPanel();
+		searchGUI.getContentPane().add(searchBarPanel, BorderLayout.NORTH);
+		
+			Label searchLabel = new Label("Search Terms");
+			searchBarPanel.add(searchLabel);
+		
+			TextField searchField = new TextField(" ", 28);
+			searchBarPanel.add(searchField);
+		
+			Button searchButton = new Button("Search");
+			searchBarPanel.add(searchButton);
+		
+		/* Low Bar */
+		
+		JPanel searchTypePanel = new JPanel();
+		searchGUI.getContentPane().add(searchTypePanel, BorderLayout.SOUTH);
+		
+			Label searchType = new Label("Search Type:");
+			searchType.setAlignment(Label.CENTER);
+			searchTypePanel.add(searchType);
+		
+			//AWT does not allow for radio buttons so checkboxes are used
+			CheckboxGroup choices = new CheckboxGroup();
+			Checkbox searchTerms = new Checkbox("Search Terms", choices, false);
+			Checkbox anySearch = new Checkbox("Any Search Terms", choices, false);
+			Checkbox exactPhrase = new Checkbox("Exact Phrase", choices, false);
+		
+			searchTypePanel.add(searchType);
+			searchTypePanel.add(searchTerms);
+			searchTypePanel.add(anySearch);
+			searchTypePanel.add(exactPhrase);
 
 	}
-	
-	
-		// Help Information Frame
-		
-		public Main (String help)
-		{
-			setTitle("Help");
-			setSize ( 600 , 400);
-			setBackground(Color.white);
-			//setLocation ( 600, 600);
-			addWindowListener( new WindowAdapter()
-			{
-				public void windowClosing( WindowEvent e)
-				{
-					dispose(); //Closes current window without closing whole program
-				}
-			});
-		}
+
 }
