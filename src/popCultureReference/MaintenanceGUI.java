2dabd21cd35e84735c9de85aadee3adeb453bcd8
againstgrizzly <brannon@outlook.com>
2015-04-11 11:41:52 -0400

    Table cells are no longer editable

--- src/popCultureReference/MaintenanceGUI.java
+++ src/popCultureReference/MaintenanceGUI.java
@@ -1,159 +1,178 @@
 package popCultureReference;
 
+import sun.java2d.pipe.SpanShapeRenderer;
+
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 interface MaintanceInterface {
 
-	  
-    void addFile();
 
-    void rebuildData();
+   void addFile();
 
-    void removeFile();
+   void rebuildData();
+
+   void removeFile();
 
 }
 
 public class MaintenanceGUI implements MaintanceInterface {
 
-	FileCreator fc = new FileCreator();
-	private JFrame frame;
+   SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+   static Object[][] dataBaseInfo = new Object[100][100];
+
+   static Object[] columns = {"Name", "Status", "Last Modified", "Path"};
+   static DefaultTableModel defaultTableModel = new DefaultTableModel(dataBaseInfo, columns){
+      @Override
+      public boolean isCellEditable(int row, int column){
+         return false;
+      }
+   };
+   private FileCreator fc = new FileCreator();
+   private JDialog frame;
 
 
-	MaintenanceGUI(){
-		System.out.println("Maintenance Initialized");
-		try {initialize();} catch (IOException e) {e.printStackTrace();}
-	}
+   MaintenanceGUI() {
+      System.out.println("Maintenance Initialized");
+      try {
+         initialize();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
 
-	public void initialize() throws IOException {
-		//table data initialization
-		frame = new JFrame();
-		frame.setTitle("Maintenance");
-		frame.setBounds(100, 100, 600, 400);
-		frame.setResizable(false);
-		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-		frame.setResizable(false);
-		frame.setLayout(new BorderLayout());
-
-		//JList
-		JPanel top = new JPanel();
-		JList list = new JList();
-		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		JScrollPane listScroller = new JScrollPane(list);
-		listScroller.setPreferredSize(new Dimension (600,350));
-		top.add(listScroller);
-
-		list.addListSelectionListener(
-				new ListSelectionListener(){
-					@Override
-					public void valueChanged(ListSelectionEvent e) {
-
-					}
-				}
-		);
-
-		frame.add(top, BorderLayout.CENTER);
-
-			//South Buttons
-		JPanel bottom = new JPanel();
-			JButton addFileButton = new JButton("Add File");
-		JButton deleteFileButton = new JButton("Delete File");
-		JButton rebuildFileButton = new JButton("Rebuild File");
-			bottom.add(addFileButton);
-			bottom.add(deleteFileButton);
-			bottom.add(rebuildFileButton);
-			frame.add(bottom, BorderLayout.SOUTH);
+   public void initialize() throws IOException {
+      //table data initialization
+      frame = new JDialog();
+      frame.setTitle("Maintenance");
+      frame.setBounds(100, 100, 600, 400);
+      frame.setResizable(false);
+      //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
+      frame.setResizable(false);
+      frame.setLayout(new BorderLayout());
 
 
+      JTable table = new JTable(defaultTableModel);
+      table.setRowHeight(30); //Will probably be the length of the array or something
+      JScrollPane scrollPane = new JScrollPane(table);
+      frame.add(scrollPane, BorderLayout.CENTER);
 
 
-		//South Button ActionListeners
-		addFileButton.addActionListener(e -> addFile());
+      //South Buttons
+      JPanel bottom = new JPanel();
+      JButton addFileButton = new JButton("Add File");
+      JButton deleteFileButton = new JButton("Delete File");
+      JButton rebuildFileButton = new JButton("Rebuild File");
+      bottom.add(addFileButton);
+      bottom.add(deleteFileButton);
+      bottom.add(rebuildFileButton);
+      frame.add(bottom, BorderLayout.SOUTH);
 
 
-		deleteFileButton.addActionListener(e -> dummy());
+      //South Button ActionListeners
+      addFileButton.addActionListener(e -> addFile());
 
 
-		rebuildFileButton.addActionListener(e -> dummy());
-
-		}
+      deleteFileButton.addActionListener(e -> dummy());
 
 
-	/*
-	Is called when the "Add File" button is clicked
-	A new instance of FileCreator is created
-	A file chooser that only allows for the selection of .txt files is brought up
-	The selected file is then passed into the method "FileWriterAwesome" which is located in the FileCreator class
+      rebuildFileButton.addActionListener(e -> dummy());
 
-	What FileWriterAwesome does:
-	Creates a file in the "File Reference Directory" folder
+   }
+
+
+   /*
+   Is called when the "Add File" button is clicked
+   A new instance of FileCreator is created
+   A file chooser that only allows for the selection of .txt files is brought up
+   The selected file is then passed into the method "FileWriterAwesome" which is located in the FileCreator class
+
+   What FileWriterAwesome does:
+   Creates a file in the "File Reference Directory" folder
     Writes the path name of the selected file to the created .txt
     Every time a new file is selected it adds a new path name
 
-	 */
-	@Override
-	public void addFile(){
+    */
+   @Override
+   public void addFile() {
 
-		/*
-		This disposes of the old frame (closes)
- 		*/
-		frame.setVisible(false);
-		frame.dispose();
+      JFileChooser chooser = new JFileChooser();
+      FileNameExtensionFilter filter = new FileNameExtensionFilter("Text Files Only", "txt");
+      chooser.setFileFilter(filter);
+      if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+         File chosenFile = chooser.getSelectedFile();
+         StringBuilder sb = new StringBuilder(chosenFile.getName());
 
-		/*
-		Brings up a filechooser that only selects .txt files
-		If the user force it to choose a non .txt file it pulls up a dialog box telling them that only text files can be selected
-		Also if they choose a non .txt, it's not added into anything (reference text, arrays, etc.)
-		 */
-		FileCreator fc = new FileCreator();
-		JFileChooser chooser = new JFileChooser();
-		FileNameExtensionFilter filter = new FileNameExtensionFilter("Text Files Only", "txt");
-		chooser.setFileFilter(filter);
-		if(chooser.showOpenDialog(null)==JFileChooser.APPROVE_OPTION){
-			File chosenFile = chooser.getSelectedFile();
-			StringBuilder sb = new StringBuilder(chosenFile.getName());
+         if (!sb.subSequence(sb.length() - 4, sb.length()).equals(".txt")) {
+            JOptionPane.showMessageDialog(null, "Only text files with the extension '.txt' can be selected.\nClose this dialog box and try again, fool!");
+         } else {
+            try {
+               fc.FileWriterAwesome(chosenFile);
+            } catch (IOException e) {
+               e.printStackTrace();
+            }
+         }
+      }
 
-			if(!sb.subSequence(sb.length()-4, sb.length()).equals(".txt")){
-				JOptionPane.showMessageDialog(null ,"Only text files with the extension '.txt' can be selected.\nClose this dialog box and try again, fool!");
-			}
-			else {
-				try {fc.FileWriterAwesome(chosenFile);} catch (IOException e) {e.printStackTrace();}
-			}
-		}
+      int iterator = 0;
 
-		/*
-		This re-initializes the updated maintenance gui so that it shows files that have been added
-		 */
-		try {
-			initialize();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+      try {
+         for(File x: fc.ArrayListCreator()){
+            dataBaseInfo[0][iterator] = x.getName();
+
+            if(x.exists() == true){
+               dataBaseInfo[1][iterator] = "Available";
+            }else{
+               dataBaseInfo[1][iterator] = "Unavailable";
+            }
+
+            dataBaseInfo[2][iterator] = sdf.format(x.lastModified());
+
+            iterator++;
+
+
+
+         }
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+
+      System.out.println(dataBaseInfo);
+
+      try {
+         frame.dispose();
+         initialize();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+
+   }
 
    @Override
    public void rebuildData() {
       // TODO Auto-generated method stub
 
    }
+
    @Override
    public void removeFile() {
       // TODO Auto-generated method stub
 
    }
 
-	public void dummy(){
-		System.out.println("Dummy");
-	}
-
+   public void dummy() {
+      System.out.println("Dummy");
+   }
 
 
 }
